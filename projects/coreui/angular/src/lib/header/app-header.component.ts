68343eea6d6e218ffc014be245f3133904451e62
xidedix <xidedix@users.noreply.github.com>
2019-02-19 15:01:31 +0100

    fix(app-header): backward compatible inputs
    - keep deprecated `navbarBrandHref `
    - use `navbarBrandRouterLink` instead

--- projects/coreui/angular/src/lib/header/app-header.component.ts
+++ projects/coreui/angular/src/lib/header/app-header.component.ts
@@ -13,6 +13,7 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   @Input() navbarBrandFull: any;
   @Input() navbarBrandMinimized: any;
   @Input() navbarBrandText: any = {icon: '🅲', text: '🅲 CoreUI'};
+  @Input() navbarBrandHref: string = ''; // deprecated, use navbarBrandRouterLink instead
   @Input() navbarBrandRouterLink: any[] | string = '';
 
   @Input() sidebarToggler: string | boolean;
@@ -36,6 +37,7 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.isFixed(this.fixed);
     this.navbarBrandImg = Boolean(this.navbarBrand || this.navbarBrandFull || this.navbarBrandMinimized);
+    this.navbarBrandRouterLink = this.navbarBrandRouterLink[0] ? this.navbarBrandRouterLink : this.navbarBrandHref;
   }
 
   ngOnDestroy(): void {
