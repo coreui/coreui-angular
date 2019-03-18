7b8df33c0d27be6a03df5ea5fd6494c25a679d94
sparun160782 <sparun1607@gmail.com>
2019-03-18 11:52:13 +0530

    Update app-breadcrumb.service.d.ts
    To Support to the current version

--- dist/@coreui/angular/lib/breadcrumb/app-breadcrumb.service.d.ts
+++ dist/@coreui/angular/lib/breadcrumb/app-breadcrumb.service.d.ts
@@ -1,5 +1,5 @@
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/index';
+import { Observable } from 'rxjs';
 export declare class AppBreadcrumbService {
     private router;
     private route;
