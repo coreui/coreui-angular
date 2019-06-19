fd10362da5a9cd7330a82171b3c4b5a0c19b298e
xidedix <xidedix@users.noreply.github.com>
2019-06-19 23:05:55 +0200

    refactor(AppHeader): add reactive toggler class based on breakpoint

--- projects/coreui/angular/src/lib/header/app-header.component.ts
+++ projects/coreui/angular/src/lib/header/app-header.component.ts
@@ -13,7 +13,7 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   @Input() navbarBrandFull: any;
   @Input() navbarBrandMinimized: any;
   @Input() navbarBrandText: any = {icon: '🅲', text: '🅲 CoreUI'};
-  @Input() navbarBrandHref: string = ''; // deprecated, use navbarBrandRouterLink instead
+  @Input() navbarBrandHref: ''; // deprecated, use navbarBrandRouterLink instead
   @Input() navbarBrandRouterLink: any[] | string = '';
 
   @Input() sidebarToggler: string | boolean;
@@ -24,6 +24,12 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
 
   private readonly fixedClass = 'header-fixed';
   navbarBrandImg: boolean;
+
+  private readonly breakpoints = ['xl', 'lg', 'md', 'sm', 'xs'];
+  sidebarTogglerClass = 'd-none d-md-block';
+  sidebarTogglerMobileClass = 'd-lg-none';
+  asideTogglerClass = 'd-none d-md-block';
+  asideTogglerMobileClass = 'd-lg-none';
 
   constructor(
     @Inject(DOCUMENT) private document: any,
@@ -38,6 +44,10 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
     this.isFixed(this.fixed);
     this.navbarBrandImg = Boolean(this.navbarBrand || this.navbarBrandFull || this.navbarBrandMinimized);
     this.navbarBrandRouterLink = this.navbarBrandRouterLink[0] ? this.navbarBrandRouterLink : this.navbarBrandHref;
+    this.sidebarTogglerClass = this.setToggerBreakpointClass(<string>this.sidebarToggler);
+    this.sidebarTogglerMobileClass = this.setToggerMobileBreakpointClass(<string>this.sidebarToggler);
+    this.asideTogglerClass = this.setToggerBreakpointClass(<string>this.asideMenuToggler);
+    this.asideTogglerMobileClass = this.setToggerMobileBreakpointClass(<string>this.asideMenuToggler);
   }
 
   ngOnDestroy(): void {
@@ -48,5 +58,22 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
     if (fixed) {
       this.renderer.addClass(this.document.body, this.fixedClass);
     }
+  }
+
+  setToggerBreakpointClass(breakpoint = 'md') {
+    let togglerClass = 'd-none d-md-block';
+    if (this.breakpoints.includes(breakpoint)) {
+      const breakpointIndex = this.breakpoints.indexOf(breakpoint);
+      togglerClass = `d-none d-${breakpoint}-block`;
+    }
+    return togglerClass;
+  }
+
+  setToggerMobileBreakpointClass(breakpoint = 'lg') {
+    let togglerClass = 'd-lg-none';
+    if (this.breakpoints.includes(breakpoint)) {
+      togglerClass = `d-${breakpoint}-none`;
+    }
+    return togglerClass;
   }
 }
