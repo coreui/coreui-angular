2b588497ba91a43c183e84521c83c4e43187e8b0
xidedix <xidedix@users.noreply.github.com>
2023-04-27 17:28:44 +0200

    feat(form-check): add reverse prop

--- projects/coreui-angular/src/lib/form/form-check/form-check.component.ts
+++ projects/coreui-angular/src/lib/form/form-check/form-check.component.ts
@@ -12,9 +12,9 @@ import { FormCheckLabelDirective } from './form-check-label.directive';
 export class FormCheckComponent implements AfterContentInit {
 
   static ngAcceptInputType_inline: BooleanInput;
+  static ngAcceptInputType_reverse: BooleanInput;
   static ngAcceptInputType_switch: BooleanInput;
 
-  private _inline = false;
   /**
    * Group checkboxes or radios on the same horizontal row.
    * @type boolean
@@ -24,9 +24,29 @@ export class FormCheckComponent implements AfterContentInit {
   set inline(value: boolean) {
     this._inline = coerceBooleanProperty(value);
   }
+
   get inline(): boolean {
     return this._inline;
   }
+
+  private _inline = false;
+
+  /**
+   * Put checkboxes or radios on the opposite side.
+   * @type boolean
+   * @default false
+   * @since 4.4.7
+   */
+  @Input()
+  set reverse(value: boolean) {
+    this._reverse = coerceBooleanProperty(value);
+  }
+
+  get reverse(): boolean {
+    return this._reverse;
+  }
+
+  private _reverse: boolean = false;
 
   /**
    * Size the component large or extra large. Works only with `[switch]="true"` [docs]
@@ -34,7 +54,7 @@ export class FormCheckComponent implements AfterContentInit {
    */
   @Input() sizing?: 'lg' | 'xl' | '' = '';
 
-    /**
+  /**
    * Render a toggle switch on for checkbox.
    * @type boolean
    * @default false
@@ -43,9 +63,11 @@ export class FormCheckComponent implements AfterContentInit {
   set switch(value: boolean) {
     this._switch = coerceBooleanProperty(value);
   }
+
   get switch(): boolean {
     return this._switch;
   }
+
   private _switch = false;
 
   @HostBinding('class')
@@ -55,11 +77,13 @@ export class FormCheckComponent implements AfterContentInit {
       'form-check': this.formCheckClass,
       'form-switch': this.switch,
       [`form-switch-${this.sizing}`]: this.switch && !!this.sizing,
-      'form-check-inline': this.inline
+      'form-check-inline': this.inline,
+      'form-check-reverse': this.reverse
     };
   }
 
   @ContentChild(FormCheckLabelDirective) formCheckLabel!: FormCheckLabelDirective;
+
   private _formCheckClass = true;
   get formCheckClass() {
     return this._formCheckClass;
