371cec81326dbdfe996a462b927d983a1d35ef3a
xidedix <xidedix@users.noreply.github.com>
2024-03-25 13:14:03 +0100

    refactor(card): improve background and text color handling with TextBgColor directive composition api

--- projects/coreui-angular/src/lib/card/card.component.ts
+++ projects/coreui-angular/src/lib/card/card.component.ts
@@ -1,14 +1,15 @@
 import { Component, HostBinding, Input } from '@angular/core';
 import { Colors, TextColors } from '../coreui.types';
-import { TextColorDirective } from '../utilities';
+import { TextBgColorDirective, TextColorDirective } from '../utilities';
 
 @Component({
   selector: 'c-card, [c-card]',
   template: '<ng-content />',
   standalone: true,
-  hostDirectives: [{
-    directive: TextColorDirective, inputs: ['cTextColor: textColor']
-  }]
+  hostDirectives: [
+    { directive: TextColorDirective, inputs: ['cTextColor: textColor'] },
+    { directive: TextBgColorDirective, inputs: ['cTextBgColor: textBgColor'] }
+  ]
 })
 export class CardComponent {
 
@@ -24,6 +25,14 @@ export class CardComponent {
    * @type TextColors
    */
   @Input() textColor?: TextColors;
+
+  /**
+   * Sets the component's color scheme to one of CoreUI's themed colors, ensuring the text color contrast adheres to the WCAG 4.5:1 contrast ratio standard for accessibility.
+   * via TextBgColorDirective
+   * @type Colors
+   * @since 5.0.0
+   */
+  @Input() textBgColor?: Colors;
 
   @HostBinding('class')
   get hostClasses(): any {
