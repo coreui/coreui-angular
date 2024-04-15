82180e5bdd23a58f35d8b47660d99e5b39dcc806
xidedix <xidedix@users.noreply.github.com>
2024-04-15 17:04:54 +0200

    fix(icon): cIcon directive [name] binding does not refresh icon for Angular 17 #203, refactor

--- projects/coreui-icons-angular/src/lib/icon/icon.utils.ts
+++ projects/coreui-icons-angular/src/lib/icon/icon.utils.ts
@@ -1,5 +1,9 @@
-export function toCamelCase(str: string) {
-  return str.replace(/([-_][a-z0-9])/ig, ($1: string) => {
+export function toCamelCase(value: string) {
+  return value.replace(/([-_][a-z0-9])/ig, ($1: string) => {
     return $1.toUpperCase().replace('-', '');
   });
+}
+
+export function transformName(value: string) {
+  return value && value.includes('-') ? toCamelCase(value) : value;
 }
