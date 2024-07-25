56e8b79431fdff7eb46986ee74270b6d0a235409
xidedix <xidedix@users.noreply.github.com>
2024-07-25 19:00:23 +0200

    refactor(schematics): add fixed dependency versions
    (cherry picked from commit 2c716b35743f3989e0cd84de138fe3434941d3cd)

--- projects/coreui-icons-angular/schematics/ng-add/index.ts
+++ projects/coreui-icons-angular/schematics/ng-add/index.ts
@@ -1,10 +1,49 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import { getPackageVersionFromPackageJson, PackageJson } from './package-config';
+import * as pkgJson from '../../package.json';
 
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    context.logger.info('Installing @coreui/icons-angular ...');
+    const pkg = pkgJson as unknown as PackageJson;
+
+    context.logger.info(``);
+    context.logger.info(`Installing ${pkg.name} dependencies...`);
+
+    const ngCoreVersionTag = getPackageVersionFromPackageJson(tree, '@angular/core');
+    context.logger.info(`@angular/core version ${ngCoreVersionTag}`);
+    if (!ngCoreVersionTag) {
+      throw new SchematicsException('@angular/core version not found');
+    }
+
+    const projectDeps: NodeDependency[] = [
+      { name: '@angular/common', type: NodeDependencyType.Default, version: ngCoreVersionTag, overwrite: false },
+      { name: '@angular/core', type: NodeDependencyType.Default, version: ngCoreVersionTag, overwrite: false },
+      {
+        name: '@angular/platform-browser',
+        type: NodeDependencyType.Default,
+        version: ngCoreVersionTag,
+        overwrite: false
+      }
+    ];
+
+    projectDeps.forEach((dep) => {
+      addPackageJsonDependency(tree, dep);
+      context.logger.info(`Added dependency: ${dep.name}@${dep.version}`);
+    });
+
+    const library: NodeDependency = {
+      name: pkg.name,
+      type: NodeDependencyType.Default,
+      version: `~${pkg.version}`,
+      overwrite: true
+    };
+
+    addPackageJsonDependency(tree, library);
+    context.logger.info(`Installing ${library.name}@${library.version}`);
     context.addTask(new NodePackageInstallTask());
+
     return tree;
   };
 }
