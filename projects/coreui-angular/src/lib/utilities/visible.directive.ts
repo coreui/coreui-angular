9b6442b85adbe48a0d2f00f23162449f5d7b053a
xidedix <xidedix@users.noreply.github.com>
2025-01-16 16:46:27 +0100

    refactor(visible): signal inputs, cleanup

--- projects/coreui-angular/src/lib/utilities/visible.directive.ts
+++ projects/coreui-angular/src/lib/utilities/visible.directive.ts
@@ -1,15 +1,31 @@
-import { Directive, inject, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[cVisible]'
+  selector: '[cVisible]',
+  exportAs: 'cVisible'
 })
+/**
+ * A directive that conditionally includes a template based on the value of an input boolean.
+ *
+ * @example
+ * ```html
+ * <ng-template [cVisible]="isVisible">Content to display</ng-template>
+ * ```
+ *
+ * @remarks
+ * This directive uses Angular's dependency injection to get references to `TemplateRef` and `ViewContainerRef`.
+ * It creates or clears the embedded view based on the value of the `cVisible` input.
+ */
 export class VisibleDirective {
   readonly #templateRef = inject<TemplateRef<any>>(TemplateRef);
   readonly #viewContainer = inject(ViewContainerRef);
 
   #hasView!: boolean;
 
-  @Input() set cVisible(condition: boolean) {
+  readonly cVisible = input<boolean>();
+
+  readonly #visibleEffect = effect(() => {
+    const condition = this.cVisible();
     if (condition && !this.#hasView) {
       this.#viewContainer.createEmbeddedView(this.#templateRef);
       this.#hasView = true;
@@ -17,5 +33,5 @@ export class VisibleDirective {
       this.#viewContainer.clear();
       this.#hasView = false;
     }
-  }
+  });
 }
