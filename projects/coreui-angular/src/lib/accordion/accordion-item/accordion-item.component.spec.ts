fb32db2ef23bdbbbee4cd47a569c6aa12740fefb
xidedix <xidedix@users.noreply.github.com>
2025-01-24 20:32:33 +0100

    test(accordion): coverage

--- projects/coreui-angular/src/lib/accordion/accordion-item/accordion-item.component.spec.ts
+++ projects/coreui-angular/src/lib/accordion/accordion-item/accordion-item.component.spec.ts
@@ -3,22 +3,24 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AccordionButtonDirective } from '../accordion-button/accordion-button.directive';
 import { AccordionService } from '../accordion.service';
 import { AccordionItemComponent } from './accordion-item.component';
+import { ComponentRef } from '@angular/core';
 
 describe('AccordionItemComponent', () => {
   let component: AccordionItemComponent;
+  let componentRef: ComponentRef<AccordionItemComponent>;
   let fixture: ComponentFixture<AccordionItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [AccordionService],
       imports: [NoopAnimationsModule, AccordionButtonDirective, AccordionItemComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccordionItemComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
     fixture.detectChanges();
   });
 
@@ -28,5 +30,13 @@ describe('AccordionItemComponent', () => {
 
   it('should have css classes', () => {
     expect(fixture.nativeElement).toHaveClass('accordion-item');
+  });
+
+  it('should toggle item', () => {
+    expect(component.visible).toBeFalse();
+    component.toggleItem();
+    expect(component.visible).toBeTrue();
+    component.toggleItem();
+    expect(component.visible).toBeFalse();
   });
 });
