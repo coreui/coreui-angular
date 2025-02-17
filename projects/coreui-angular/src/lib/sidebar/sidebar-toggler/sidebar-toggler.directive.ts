05740d3a234ab5600b2a1963a6cb3b807fdfac9b
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:20:46 +0100

    refactor(sidebar-toggler): signal inputs, host bindings, cleanup

--- projects/coreui-angular/src/lib/sidebar/sidebar-toggler/sidebar-toggler.directive.ts
+++ projects/coreui-angular/src/lib/sidebar/sidebar-toggler/sidebar-toggler.directive.ts
@@ -1,18 +1,18 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import { SidebarToggleDirective } from '../sidebar-toggle/sidebar-toggle.directive';
 
 @Directive({
   selector: '[cSidebarToggler]',
-  hostDirectives: [{ directive: SidebarToggleDirective, inputs: ['cSidebarToggle: cSidebarToggler', 'toggle'] }]
+  hostDirectives: [{ directive: SidebarToggleDirective, inputs: ['cSidebarToggle: cSidebarToggler', 'toggle'] }],
+  host: {
+    '[attr.role]': 'role()',
+    class: 'sidebar-toggler',
+    '[style]': 'getStyles'
+  }
 })
 export class SidebarTogglerDirective {
-  @HostBinding('attr.role')
-  @Input()
-  role = 'button';
+  readonly role = input('button');
 
-  @HostBinding('class.sidebar-toggler') sidebarTogglerClass = true;
-
-  @HostBinding('style')
   get getStyles(): any {
     return {
       appearance: 'button',
