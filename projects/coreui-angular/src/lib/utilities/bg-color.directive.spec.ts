45f9dc552e8bdbca6b594238539a78809df85da2
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:13:21 +0100

    test(bg-color): css classes coverage

--- projects/coreui-angular/src/lib/utilities/bg-color.directive.spec.ts
+++ projects/coreui-angular/src/lib/utilities/bg-color.directive.spec.ts
@@ -1,11 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BgColorDirective } from './bg-color.directive';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  imports: [BgColorDirective],
+  template: '<div cBgColor="primary"></div>'
+})
+class TestComponent {}
 
 describe('BgColorDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.directive(BgColorDirective));
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
     TestBed.runInInjectionContext(() => {
-    const directive = new BgColorDirective();
-    expect(directive).toBeTruthy();
+      const directive = new BgColorDirective();
+      expect(directive).toBeTruthy();
     });
+  });
+
+  it('should have css classes', () => {
+    expect(debugElement.nativeElement).toHaveClass('bg-primary');
   });
 });
