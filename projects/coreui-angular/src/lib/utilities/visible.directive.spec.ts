4b8d14b64a3174c89971bf53f6f00036eab62698
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:18:35 +0100

    test(visible): coverage

--- projects/coreui-angular/src/lib/utilities/visible.directive.spec.ts
+++ projects/coreui-angular/src/lib/utilities/visible.directive.spec.ts
@@ -1,13 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { VisibleDirective } from './visible.directive';
-import { TestBed } from '@angular/core/testing';
-import { TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Component({
+  imports: [VisibleDirective],
+  template: '<ng-template [cVisible]="visible()">Test Node</ng-template>'
+})
+class TestComponent {
+  readonly visible = input(true);
+}
 
 describe('VisibleDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [TestComponent],
       providers: [TemplateRef, ViewContainerRef]
-    });
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.directive(VisibleDirective));
+    fixture.detectChanges();
   });
+
   it('should create an instance', () => {
     TestBed.runInInjectionContext(() => {
       const directive = new VisibleDirective();
