053afba2fe738b009ae11c6f1c7f21b236e0d467
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:08:02 +0100

    refactor(form): host binding, cleanup, tests

--- projects/coreui-angular/src/lib/form/form/form.directive.spec.ts
+++ projects/coreui-angular/src/lib/form/form/form.directive.spec.ts
@@ -1,11 +1,48 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, ComponentRef, DebugElement, input } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { FormDirective } from './form.directive';
-import { TestBed } from '@angular/core/testing';
+
+@Component({
+  imports: [FormDirective],
+  template: '<form cForm [validated]="validated()"></form>'
+})
+class TestComponent {
+  readonly validated = input(false);
+}
 
 describe('FormDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+  let componentRef: ComponentRef<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    debugElement = fixture.debugElement.query(By.directive(FormDirective));
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
     TestBed.runInInjectionContext(() => {
       const directive = new FormDirective();
       expect(directive).toBeTruthy();
     });
+  });
+
+  it('should have css classes', () => {
+    expect(debugElement.nativeElement).not.toHaveClass('was-validated');
+    componentRef.setInput('validated', true);
+    fixture.detectChanges();
+    expect(debugElement.nativeElement).toHaveClass('was-validated');
+    componentRef.setInput('validated', false);
+    fixture.detectChanges();
+    expect(debugElement.nativeElement).not.toHaveClass('was-validated');
   });
 });
--- projects/coreui-angular/src/lib/form/form/form.directive.ts
+++ projects/coreui-angular/src/lib/form/form/form.directive.ts
@@ -1,20 +1,14 @@
-import { booleanAttribute, computed, Directive, input } from '@angular/core';
+import { booleanAttribute, Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'form[cForm]',
-  host: { '[class]': 'hostClasses()' }
+  host: { '[class.was-validated]': 'validated()' }
 })
 export class FormDirective {
   /**
    * Mark a form as validated. If you set it `true`, all validation styles will be applied to the form. [docs]
-   * @type boolean
+   * @return boolean
    * @default false
    */
   readonly validated = input(false, { transform: booleanAttribute });
-
-  readonly hostClasses = computed(() => {
-    return {
-      'was-validated': this.validated()
-    } as Record<string, boolean>;
-  });
 }
