4cd332c01aeb2aa2eb8a73f2c7f4875aa1207f2e
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:15:16 +0100

    test(rounded): css classes coverage

--- projects/coreui-angular/src/lib/utilities/rounded.directive.spec.ts
+++ projects/coreui-angular/src/lib/utilities/rounded.directive.spec.ts
@@ -1,11 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RoundedDirective } from './rounded.directive';
+import { Component, DebugElement, input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  imports: [RoundedDirective],
+  template: '<div [cRounded]="rounded()"></div>'
+})
+class TestComponent {
+  readonly rounded = input(1);
+}
 
 describe('RoundedDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.directive(RoundedDirective));
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
     TestBed.runInInjectionContext(() => {
-    const directive = new RoundedDirective();
-    expect(directive).toBeTruthy();
+      const directive = new RoundedDirective();
+      expect(directive).toBeTruthy();
     });
+  });
+
+  it('should have css classes', () => {
+    expect(debugElement.nativeElement).toHaveClass('rounded-1');
+    fixture.componentRef.setInput('rounded', true);
+    fixture.detectChanges();
+    expect(debugElement.nativeElement).toHaveClass('rounded');
+    fixture.componentRef.setInput('rounded', {
+      top: false,
+      end: true,
+      circle: true,
+      pill: true,
+      size: 3
+    });
+    fixture.detectChanges();
+    expect(debugElement.nativeElement).toHaveClass('rounded-3');
+    expect(debugElement.nativeElement).toHaveClass('rounded-end');
+    expect(debugElement.nativeElement).toHaveClass('rounded-circle');
+    expect(debugElement.nativeElement).toHaveClass('rounded-pill');
+    expect(debugElement.nativeElement.classList.length).toBe(4);
+    fixture.componentRef.setInput('rounded', {});
+    fixture.detectChanges();
+    expect(debugElement.nativeElement.classList.length).toBe(0);
+    fixture.componentRef.setInput('rounded', 1234n);
+    fixture.detectChanges();
+    expect(debugElement.nativeElement.classList.length).toBe(0);
   });
 });
