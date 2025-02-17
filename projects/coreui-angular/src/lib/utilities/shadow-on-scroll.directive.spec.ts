bdb21648c5a82d982ea0e463386da4dca4be0ec4
xidedix <xidedix@users.noreply.github.com>
2025-02-17 16:18:15 +0100

    test(shadow-on-scroll): coverage

--- projects/coreui-angular/src/lib/utilities/shadow-on-scroll.directive.spec.ts
+++ projects/coreui-angular/src/lib/utilities/shadow-on-scroll.directive.spec.ts
@@ -1,16 +1,34 @@
-import { ElementRef } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { Component, DebugElement, ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShadowOnScrollDirective } from './shadow-on-scroll.directive';
+import { By } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  imports: [ShadowOnScrollDirective],
+  template: '<div cShadowOnScroll></div>'
+})
+class TestComponent {}
 
 class MockElementRef extends ElementRef {}
 
 describe('ShadowOnScrollDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+  let document: Document;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ShadowOnScrollDirective],
+      imports: [TestComponent, ShadowOnScrollDirective],
       providers: [{ provide: ElementRef, useClass: MockElementRef }]
-    });
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    document = TestBed.inject(DOCUMENT);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.directive(ShadowOnScrollDirective));
+    fixture.detectChanges();
   });
 
   it('should create an instance', () => {
