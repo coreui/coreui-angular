6cd3cd4d3726ba74996a477503adf38fa2514e24
xidedix <xidedix@users.noreply.github.com>
2025-02-25 12:06:36 +0100

    fix(align): allow undefined type for align input prop

--- projects/coreui-angular/src/lib/utilities/align.directive.ts
+++ projects/coreui-angular/src/lib/utilities/align.directive.ts
@@ -11,7 +11,7 @@ export class AlignDirective {
    * Set vertical alignment of inline, inline-block, inline-table, and table cell elements
    * @return Alignment
    */
-  readonly align = input<Alignment>(undefined, { alias: 'cAlign' });
+  readonly align = input<Alignment | undefined>(undefined, { alias: 'cAlign' });
 
   readonly hostClasses = computed(() => {
     const align = this.align();
