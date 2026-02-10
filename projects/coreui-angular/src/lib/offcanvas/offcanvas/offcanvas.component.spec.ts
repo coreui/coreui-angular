3270519e94e97e5d6c07dca9343da141a38c33fa
xidedix <xidedix@users.noreply.github.com>
2026-02-10 15:55:47 +0100

    test(offcanvas): update tests

--- projects/coreui-angular/src/lib/offcanvas/offcanvas/offcanvas.component.spec.ts
+++ projects/coreui-angular/src/lib/offcanvas/offcanvas/offcanvas.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentRef, DOCUMENT } from '@angular/core';
 import { ComponentFixture, fakeAsync, flushMicrotasks, TestBed, tick } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { OffcanvasComponent } from './offcanvas.component';
 
@@ -12,7 +11,7 @@ describe('OffcanvasComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, OffcanvasComponent]
+      imports: [OffcanvasComponent]
     }).compileComponents();
 
     fixture = TestBed.createComponent(OffcanvasComponent);
@@ -20,6 +19,15 @@ describe('OffcanvasComponent', () => {
     componentRef = fixture.componentRef;
     document = TestBed.inject(DOCUMENT);
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    componentRef.setInput('visible', false);
+    fixture.detectChanges();
+    const backdrop = document.querySelector('.offcanvas-backdrop');
+    if (backdrop) {
+      document.body.removeChild(backdrop);
+    }
   });
 
   it('should create', () => {
@@ -34,6 +42,7 @@ describe('OffcanvasComponent', () => {
 
   it('should react to visible changes', fakeAsync(() => {
     expect(componentRef.instance.visible()).toBeFalse();
+    expect(fixture.nativeElement.getAttribute('inert')).toBe('true');
     componentRef.setInput('visible', true);
     fixture.detectChanges();
     flushMicrotasks();
@@ -45,6 +54,7 @@ describe('OffcanvasComponent', () => {
     componentRef.setInput('visible', true);
     fixture.detectChanges();
     expect(componentRef.instance.visible()).toBeTrue();
+    expect(fixture.nativeElement.getAttribute('inert')).toBeNull();
     document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
     tick();
     fixture.detectChanges();
@@ -53,18 +63,22 @@ describe('OffcanvasComponent', () => {
   }));
 
   it('should close offcanvas on backdrop click', fakeAsync(() => {
+    componentRef.setInput('backdrop', true);
     componentRef.setInput('visible', true);
     fixture.detectChanges();
     expect(componentRef.instance.visible()).toBeTrue();
+    expect(componentRef.location.nativeElement.classList).toContain('show');
     const backdrop = document.querySelector('.offcanvas-backdrop');
     expect(backdrop).not.toBeNull();
-    if (backdrop) {
-      backdrop?.dispatchEvent(new MouseEvent('click'));
-      tick();
-      fixture.detectChanges();
-      // expect(componentRef.instance.visible()).toBeFalse();
-      // expect(fixture.nativeElement.getAttribute('inert')).toBeTruthy();
-    }
+    tick(300);
+    // console.log(backdrop);
+    expect(backdrop).toHaveClass('show');
+    backdrop?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    tick(300);
+    fixture.detectChanges();
+    tick(300);
+    // console.log(backdrop);
+    expect(componentRef.location.nativeElement).not.toHaveClass('show');
   }));
 
   it('should return breakpoint value', fakeAsync(() => {
