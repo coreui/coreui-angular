0b049187f7fe27b30cbb1977f9981da2f34e9d66
xidedix <xidedix@users.noreply.github.com>
2026-02-13 18:58:20 +0100

    refactor(toast): migrate to native CSS animations
    potential issues:
    - new css requires ViewEncapsulation.None
    - to make grid animation work add `c-toast-content` wrapper

--- projects/coreui-angular/src/lib/toast/toast/toast.component.ts
+++ projects/coreui-angular/src/lib/toast/toast/toast.component.ts
@@ -1,8 +1,10 @@
 import {
+  AnimationCallbackEvent,
   booleanAttribute,
   ChangeDetectorRef,
   Component,
   computed,
+  DestroyRef,
   effect,
   ElementRef,
   inject,
@@ -12,51 +14,40 @@ import {
   OnDestroy,
   OnInit,
   output,
-  Renderer2
+  Renderer2,
+  signal,
+  ViewEncapsulation
 } from '@angular/core';
-
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { skipWhile } from 'rxjs';
 
 import { Colors } from '../../coreui.types';
 import { ToasterService } from '../toaster/toaster.service';
 import { TToasterPlacement } from '../toaster/toaster.component';
-
-type AnimateType = 'hide' | 'show';
+import { ToastContentComponent } from './toast-content.component';
 
 @Component({
   selector: 'c-toast',
-  template: '<ng-content />',
+  template: `
+    <c-toast-content>
+      <ng-content />
+    </c-toast-content>
+  `,
   styleUrls: ['./toast.component.scss'],
   exportAs: 'cToast',
-  animations: [
-    trigger('fadeInOut', [
-      state('show', style({ opacity: 1, height: '*', padding: '*', border: '*', margin: '*' })),
-      state('hide', style({ opacity: 0, height: 0, padding: 0, border: 0, margin: 0 })),
-      state('void', style({ opacity: 0, height: 0, padding: 0, border: 0, margin: 0 })),
-      transition('show => hide', [animate('{{ time }} {{ easing }}')], {
-        params: { time: '300ms', easing: 'ease-out' }
-      }),
-      transition('hide => show', [animate('{{ time }} {{ easing }}')], {
-        params: { time: '300ms', easing: 'ease-in' }
-      }),
-      transition('show => void', [animate('{{ time }} {{ easing }}')], {
-        params: { time: '300ms', easing: 'ease-out' }
-      }),
-      transition('void => show', [animate('{{ time }} {{ easing }}')], {
-        params: { time: '300ms', easing: 'ease-in' }
-      })
-    ])
-  ],
   host: {
-    class: 'toast show',
+    class: 'toast',
     '[class]': 'hostClasses()',
+    '(animate.enter)': 'handleEnter($event)',
+    '(animate.leave)': 'handleLeave($event)',
     '(mouseover)': 'clearTimer()',
-    '(mouseout)': 'setTimer()',
-    '[@fadeInOut]': 'animateType',
-    '[@.disabled]': 'animationDisabled()'
-  }
+    '(mouseout)': 'setTimer()'
+  },
+  encapsulation: ViewEncapsulation.None,
+  imports: [ToastContentComponent],
 })
 export class ToastComponent implements OnInit, OnDestroy {
+  readonly destroyRef = inject(DestroyRef);
   readonly changeDetectorRef = inject(ChangeDetectorRef);
   readonly hostElement = inject(ElementRef);
   readonly renderer = inject(Renderer2);
@@ -149,21 +140,15 @@ export class ToastComponent implements OnInit, OnDestroy {
     this.changeDetectorRef.markForCheck();
   }
 
-  readonly animationDisabled = computed(() => {
-    return !this.fade();
-  });
-
-  get animateType(): AnimateType {
-    return this.visible ? 'show' : 'hide';
-  }
-
   readonly hostClasses = computed(() => {
     const color = this.color();
     return {
       toast: true,
-      show: true,
-      [`bg-${color}`]: !!color,
-      'border-0': !!color
+      show: this.#visible() && this.#rendered(),
+      [`text-bg-${color}`]: !!color,
+      // [`bg-${color}`]: !!color,
+      'border-0': !!color,
+      fade: this.fade()
     } as Record<string, boolean>;
   });
 
@@ -222,5 +207,25 @@ export class ToastComponent implements OnInit, OnDestroy {
     clearTimeout(this.clockTimerId);
     clearInterval(this.clockId);
     this.clockId = undefined;
+  }
+
+  readonly #rendered = signal(false);
+
+  protected handleEnter($event: AnimationCallbackEvent) {
+    // Check if the component is inserted dynamically / conditionally - the class '.ng-star-inserted' is added to the host element.
+    // If the class is present, we can set the visible state to true to trigger the animation.
+    const inserted = this.hostElement.nativeElement.classList.contains('ng-star-inserted');
+    if (inserted) {
+      this.#visible.set(true);
+    }
+    setTimeout(() => {
+      this.#rendered.set(true);
+    });
+  }
+  protected handleLeave($event: AnimationCallbackEvent) {
+    this.#visible.set(false);
+    // component is destroyed already, remove the class manually
+    this.renderer.removeClass(this.hostElement.nativeElement, 'show');
+    // console.log('Leaving event:', { ...$event });
   }
 }
